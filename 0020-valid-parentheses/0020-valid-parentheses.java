95d785da9546c837e945569e30c5c42ddf236a79
PrathyushaBatchalakuri <batchalakuriprathyusha9@gmail.com>
2025-02-25 19:08:09 -0500

    Time: 3 ms (41.98%), Space: 41.7 MB (65.28%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,50 +1,38 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length() == 1)
+
+        //if there is only one bracket in the string s.
+        if( s.length() == 1 )
             return false;
         
-        Stack<Character> sc = new Stack<>();
+        HashMap<Character, Character> map = new HashMap<>();
+            map.put(')','(');
+            map.put('}','{');
+            map.put(']','[');
 
+        Stack<Character> mainStack = new Stack<>();
 
-        for( int i = 0; i < s.length(); i++ ){
+        for(int i = 0; i < s.length(); i++){
 
-            if(s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{' ){
-                
-                sc.push(s.charAt(i));
+            if(s.charAt(i) == '(' || s.charAt(i) == '{' || s.charAt(i) == '['){
+
+                mainStack.push(s.charAt(i)); //push the open brackets onto the stack
 
             }
 
             else{
 
-                if(sc.isEmpty())
-                    return false;
-                if(s.charAt(i) == ')' && sc.peek() != '(' ){
-                    
-                    return false;
+                if(mainStack.isEmpty() || map.get(s.charAt(i)) != mainStack.peek() ){
+                    return false; //if the stack is empty OR when the peek of the stack does not match the key-value pair, return false
                 }
-                
-                else if(s.charAt(i) == '}' && sc.peek() != '{' ){
-                    System.out.print("Hi");
-                    
-                    return false;
-                }
-                
-                else if(s.charAt(i) == ']' && sc.peek() != '[' ){
 
-                    System.out.print("Hi");
-                    
-                    return false;
-                }
-                else{
-                    sc.pop();
-                    continue;
-                }
+                mainStack.pop(); // if not, you found a pair. Pop the already paired bracket out of stack.
+
             }
 
         }
-
-        if(!sc.isEmpty())
-            return false;
-        return true;
-    }
+    
+        return mainStack.isEmpty(); // Finally, if the stack is completely empty, return true. Else return false
+    
+    }    
 }
